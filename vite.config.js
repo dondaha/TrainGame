b9d107502b90ce592f39e1094d29efdec3010fb0
dondaha <1656982710@qq.com>
2024-11-17 20:27:09 +0800

    部署版

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   assetsInclude: ['**/*.glb'],
+  base: "/TrainGame/",
+  build: {
+    outDir: "docs"
+  },
 })
